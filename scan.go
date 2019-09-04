b1840dd72b6250ce8b2131fc0018811293c53842
Carey McLelland <carey.mclelland@cbc.ca>
2019-09-04 13:56:36 -0400

    Remove closed output.

--- scan.go
+++ scan.go
@@ -45,8 +45,6 @@ func ScanPort(ip string, port int, timeout time.Duration) {
 		if strings.Contains(err.Error(), "too many open files") {
 			time.Sleep(timeout)
 			ScanPort(ip, port, timeout)
-			//} else {
-			//	fmt.Println(port, "closed")
 		}
 		return
 	}
