b20360d5b36a43574e608a0d13330d5fadc89e1a
Carey McLelland <carey.mclelland@cbc.ca>
2019-09-25 16:07:34 -0400

    refactor.

--- cmd/root.go
+++ cmd/root.go
@@ -5,15 +5,17 @@ import (
 	"log"
 	"os"
 
+	"github.com/mclellac/scanner"
 	"github.com/urfave/cli"
 )
 
-func main() {
+func Run() {
 	app := cli.NewApp()
-	app.Name = "boom"
-	app.Usage = "make an explosive entrance"
+	app.Name = "tripsix"
+	app.Usage = "Quick and simple port scanner."
 	app.Action = func(c *cli.Context) error {
 		fmt.Println("boom! I say!")
+		scanner.Start()
 		return nil
 	}
 
--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "github.com/mclellac/tripsix/cmd"
 
 func main() {
-	cmd.Execute()
+	cmd.Run()
 }
